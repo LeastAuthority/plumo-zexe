584e86d8e9ceb65f4d277f15fe81d1b786a1bb52
keks <keks@cryptoscope.co>
2020-10-28 20:39:23 +0100

    some fixes, doens't work yet

--- algebra-audit/src/lib.rs
+++ algebra-audit/src/lib.rs
@@ -1,13 +1,12 @@
-use algebra::bw6_761::g1::{
-        Parameters,
-        G1_GENERATOR_X as Gx,
-        G1_GENERATOR_Y as Gy,
-    };
-//use   algebra_core::models::GroupAffine;
-use   algebra_core::GroupAffine;
-
 #[cfg(test)]
 mod tests {
+    use algebra::bw6_761::g1::{
+            Parameters,
+            G1_GENERATOR_X as Gx,
+            G1_GENERATOR_Y as Gy,
+        };
+    use algebra_core::curves::models::short_weierstrass_projective::GroupAffine;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -21,6 +20,7 @@ mod tests {
             infinity: false,
             x: Gx,
             y: Gy,
+            _params: {},
         };
 
         assert_eq!(2 + 2, 4);
