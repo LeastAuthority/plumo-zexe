ef47680ceff095cc5f9ff8b8e2e139dc0a745dfd
Ann-Christine Kycler <ann-christine.kycler@tuhh.de>
2020-11-01 17:21:02 +0100

    Addes tests for field operations with the neutral element

--- algebra-audit/src/lib.rs
+++ algebra-audit/src/lib.rs
@@ -1,9 +1,31 @@
 #[cfg(test)]
 mod tests {
-    use algebra::bw6_761::g1::{Parameters, G1_GENERATOR_X as Gx, G1_GENERATOR_Y as Gy};
-    use algebra_core::curves::{
-        batch_arith::BatchGroupArithmetic, models::short_weierstrass_projective::GroupAffine,
+    use algebra::bw6_761::{
+        fq::*,
+        fq3::*,
+        g1::{Parameters, G1_GENERATOR_X as Gx, G1_GENERATOR_Y as Gy}};
+    use algebra_core::{
+        biginteger::{BigInteger, BigInteger768},
+        fields::{
+            FftField, FftParameters, Field, Fp12Parameters, Fp2Parameters, Fp6Parameters, FpParameters,
+            SquareRootField,arithmetic,
+        },
+        curves::{
+            batch_arith::BatchGroupArithmetic, models::short_weierstrass_projective::GroupAffine,
+        },
+        One, UniformRand, Zero,
     };
+    
+    use core::{
+        cmp::Ordering,
+        ops::{AddAssign, MulAssign, SubAssign},
+    };
+
+    use std::ptr;
+    
+  /*   use crate::bw6_761::{
+        Fq, Fq3, Fq3Parameters, Fq6, Fq6Parameters, FqParameters, Fr
+    }; */
 
     #[test]
     fn batch_add_in_place_happy_case() {
@@ -249,4 +271,87 @@ mod tests {
         assert_eq!(res[0], g+g);   // elems[0] + elems[0]
         assert_eq!(res[1], g+g+g); // elems[0] + elems[1]
     }
+
+    #[test]
+    fn field_add_assign_fp768_test() {
+        //define Field element (generator from G1)
+        let a = Fq::new(BigInteger768([
+            289919226011913130u64,
+        13019990545710127566u64,
+        4409829457611675068u64,
+        13030600802816293865u64,
+        15696054586628993047u64,
+        9353078419867322391u64,
+        5664203968291172875u64,
+        5090703637405909511u64,
+        17774776443174359288u64,
+        10018561694451762270u64,
+        12632664537138156478u64,
+        46143195394855163u64,
+        ]));
+        let mut result = a;
+        result.add_assign(Fq::new(BigInteger768([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+        //Assert if adding the neutral element to a field element returns the same element
+        assert_eq!(result, a);
+    }
+
+    #[test]
+    fn field_sub_assign_fp768_test() {
+        //define Field element (generator from G1)
+        let a = Fq::new(BigInteger768([
+            289919226011913130u64,
+        13019990545710127566u64,
+        4409829457611675068u64,
+        13030600802816293865u64,
+        15696054586628993047u64,
+        9353078419867322391u64,
+        5664203968291172875u64,
+        5090703637405909511u64,
+        17774776443174359288u64,
+        10018561694451762270u64,
+        12632664537138156478u64,
+        46143195394855163u64,
+        ]));
+        let mut result = a;
+        result.sub_assign(Fq::new(BigInteger768([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+        //Assert if substracting the neutral element to a field element returns the same element
+        assert_eq!(result, a);
+    }
+
+    #[test]
+    fn field_mul_assign_fp768_test() {
+        //define Field element (generator from G1)
+        let a = Fq::new(BigInteger768([
+            289919226011913130u64,
+        13019990545710127566u64,
+        4409829457611675068u64,
+        13030600802816293865u64,
+        15696054586628993047u64,
+        9353078419867322391u64,
+        5664203968291172875u64,
+        5090703637405909511u64,
+        17774776443174359288u64,
+        10018561694451762270u64,
+        12632664537138156478u64,
+        46143195394855163u64,
+        ]));
+        let mut result = a;
+        result.mul_assign(Fq::new(BigInteger768([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+        //Assert if mulitplying the neutral element to a field element returns the neutral element
+        assert_eq!(result, Fq::new(BigInteger768([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])));
+    }
+    
+/*     #[test]
+    fn modsub768_test() {
+        let mut a = ptr::null();
+        #[cfg(use_bw6_asm)]
+        #[allow(unsafe_code, unused_mut, unconditional_panic)]
+        algebra_core::fields::arithmetic::modmul768(
+            a,
+            a,
+            a,
+            a,
+        );
+        assert_eq!(a, ptr::null());
+    } */
 }
