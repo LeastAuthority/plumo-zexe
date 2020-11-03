249fc48e5d3ef8730bb0c4dd6bbdf64f8a9ce786
keks <keks@cryptoscope.co>
2020-11-03 13:33:22 +0100

    add more audit tests

--- algebra-audit/src/lib.rs
+++ algebra-audit/src/lib.rs
@@ -9,12 +9,12 @@ mod tests {
     fn batch_add_in_place_happy_case() {
         type Group = GroupAffine<Parameters>;
 
-        let G = Group::new(Gx, Gy, false);
+        let g = Group::new(Gx, Gy, false);
 
-        // these are all [[2]G, [3]G]
-        let mut bases = [G + G, G + G + G];
-        let mut others = [G + G, G + G + G];
-        let mut bases_same_slice = [G + G, G + G + G];
+        // these are all [[2]g, [3]g]
+        let mut bases = [g + g, g + g + g];
+        let mut others = [g + g, g + g + g];
+        let mut bases_same_slice = [g + g, g + g + g];
 
         BatchGroupArithmetic::batch_add_in_place(&mut bases, &mut others, &[(0, 1)]);
         BatchGroupArithmetic::batch_add_in_place_same_slice(&mut bases_same_slice, &[(0, 1)]);
@@ -25,35 +25,33 @@ mod tests {
 
     #[test]
     fn batch_add_more_than_once_same_slice() {
-       
         type Group = GroupAffine<Parameters>;
-        let G = Group::new(Gx, Gy, false);
+        let g = Group::new(Gx, Gy, false);
 
-        let mut group_elem_bases = [G + G, G + G + G];
-        let mut group_elem_bases_steps = [G + G, G + G + G];
+        let mut group_elem_bases_steps = [g + g, g + g + g];
 
-       // BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases, &[(0,0),(0,1)]);
-        BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases_steps, &[(1,0)]);
-        BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases_steps, &[(1,1)]);
-        assert_eq!(group_elem_bases_steps[1], G+G+G+G+G+G+G);
-     //   assert_eq!(group_elem_bases[0], G+G+G+G+G+G+G);
+        // BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases, &[(0,0),(0,1)]);
+        BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases_steps, &[(1, 0)]);
+        BatchGroupArithmetic::batch_add_in_place_same_slice(&mut group_elem_bases_steps, &[(1, 1)]);
+        assert_eq!(group_elem_bases_steps[1], g + g + g + g + g + g + g);
+        //   assert_eq!(group_elem_bases[0], g+g+g+g+g+g+g);
     }
 
     #[test]
-   fn batch_add_in_place_reuse_bases_0() {
+    fn batch_add_in_place_reuse_bases_0() {
         type Group = GroupAffine<Parameters>;
 
-        let G = Group::new(Gx, Gy, false);
+        let g = Group::new(Gx, Gy, false);
 
-        let mut group_elem_bases = [G + G, G + G + G];
-        let mut group_elem_bases_steps = [G + G, G + G + G];
+        let mut group_elem_bases = [g + g, g + g + g];
+        let mut group_elem_bases_steps = [g + g, g + g + g];
 
         // made several copies because at some point they say that the
         // second parameter to the function becomes a junk value after
         // the function was called.
-        let mut group_elem_others = [G + G, G];
-        let mut group_elem_others_steps1 = [G + G, G];
-        let mut group_elem_others_steps2 = [G + G, G];
+        let mut group_elem_others = [g + g, g];
+        let mut group_elem_others_steps1 = [g + g, g];
+        let mut group_elem_others_steps2 = [g + g, g];
 
         BatchGroupArithmetic::batch_add_in_place(
             &mut group_elem_bases,
@@ -73,28 +71,28 @@ mod tests {
 
         // Asserts if one call of batch_add_in_place with multiple indices results in the same
         // thing as two successive function calls with different indices.
-        assert_eq!(group_elem_bases_steps[0], G + G + G + G + G);
+        assert_eq!(group_elem_bases_steps[0], g + g + g + g + g);
 
         // This one fails! This means that an index list that contains two elements
         // (x1, y1), (x2, y2), x1==x2 causes trouble.
-        assert_eq!(group_elem_bases[0], G + G + G + G + G);
+        assert_eq!(group_elem_bases[0], g + g + g + g + g);
     }
 
     #[test]
     fn batch_add_in_place_reuse_bases_1() {
         type Group = GroupAffine<Parameters>;
 
-        let G = Group::new(Gx, Gy, false);
+        let g = Group::new(Gx, Gy, false);
 
-        let mut group_elem_bases = [G + G, G + G + G];
-        let mut group_elem_bases_steps = [G + G, G + G + G];
+        let mut group_elem_bases = [g + g, g + g + g];
+        let mut group_elem_bases_steps = [g + g, g + g + g];
 
         // made several copies because at some point they say that the
         // second parameter to the function becomes a junk value after
         // the function was called.
-        let mut group_elem_others = [G + G, G];
-        let mut group_elem_others_steps1 = [G + G, G];
-        let mut group_elem_others_steps2 = [G + G, G];
+        let mut group_elem_others = [g + g, g];
+        let mut group_elem_others_steps1 = [g + g, g];
+        let mut group_elem_others_steps2 = [g + g, g];
 
         BatchGroupArithmetic::batch_add_in_place(
             &mut group_elem_bases,
@@ -114,28 +112,28 @@ mod tests {
 
         // Asserts if one call of batch_add_in_place with multiple indices results in the same
         // thing as two successive function calls with different indices.
-        assert_eq!(group_elem_bases_steps[1], G + G + G + G + G + G);
+        assert_eq!(group_elem_bases_steps[1], g + g + g + g + g + g);
 
         // This one fails! This means that an index list that contains two elements
         // (x1, y1), (x2, y2), x1==x2 causes trouble.
-        assert_eq!(group_elem_bases[1], G + G + G + G + G + G);
+        assert_eq!(group_elem_bases[1], g + g + g + g + g + g);
     }
 
     #[test]
     fn batch_add_in_place_reuse_others() {
         type Group = GroupAffine<Parameters>;
 
-        let G = Group::new(Gx, Gy, false);
+        let g = Group::new(Gx, Gy, false);
 
-        let mut group_elem_bases = [G + G, G + G + G];
-        let mut group_elem_bases_steps = [G + G, G + G + G];
+        let mut group_elem_bases = [g + g, g + g + g];
+        let mut group_elem_bases_steps = [g + g, g + g + g];
 
         // made several copies because at some point they say that the
         // second parameter to the function becomes a junk value after
         // the function was called.
-        let mut group_elem_others = [G + G, G];
-        let mut group_elem_others_steps1 = [G + G, G];
-        let mut group_elem_others_steps2 = [G + G, G];
+        let mut group_elem_others = [g + g, g];
+        let mut group_elem_others_steps1 = [g + g, g];
+        let mut group_elem_others_steps2 = [g + g, g];
 
         BatchGroupArithmetic::batch_add_in_place(
             &mut group_elem_bases,
@@ -155,10 +153,100 @@ mod tests {
 
         // Asserts if one call of batch_add_in_place with multiple indices results in the same
         // thing as two successive function calls with different indices.
-        assert_eq!(group_elem_bases_steps, [G + G + G + G, G + G + G + G + G]);
+        assert_eq!(group_elem_bases_steps, [g + g + g + g, g + g + g + g + g]);
 
         // This one fails! This means that an index list that contains two elements
         // (x1, y1), (x2, y2), y1==y2 causes trouble.
-        assert_eq!(group_elem_bases, [G + G + G + G, G + G + G + G + G]);
+        assert_eq!(group_elem_bases, [g + g + g + g, g + g + g + g + g]);
+    }
+
+    #[test]
+    fn bucketed_add_panic_1() {
+        use algebra_core::curves::bucketed_add::{batch_bucketed_add, BucketPosition};
+
+        type Group = GroupAffine<Parameters>;
+
+        let g = Group::new(Gx, Gy, false);
+
+        let buckets = 1;
+
+        // these are all [[2]g, [3]g]
+        let elems = [g + g, g + g + g];
+
+        // panic on single bucket position
+        let mut bucket_positions = [BucketPosition {
+            bucket: 0,
+            position: 0,
+        }];
+
+        batch_bucketed_add(buckets, &elems, &mut bucket_positions);
+    }
+
+    #[test]
+    fn bucketed_add_panic_2() {
+        use algebra_core::{
+            curves::bucketed_add::{batch_bucketed_add, BucketPosition},
+        };
+
+        let g = GroupAffine::<Parameters>::new(Gx, Gy, false);
+
+        let buckets = 2;
+
+        // these are all [[1]g, [2]g, [3]g, [4]g]
+        let elems = [g, g + g, g + g + g, g + g + g + g];
+
+        // panic on position reuse
+        let mut bucket_positions = [
+            BucketPosition {
+                bucket: 0,
+                position: 0,
+            },
+            BucketPosition {
+                bucket: 1,
+                position: 0,
+            },
+        ];
+
+        batch_bucketed_add(buckets, &elems, &mut bucket_positions);
+    }
+
+    #[test]
+    fn bucketed_add_works() {
+        use algebra_core::{
+            curves::bucketed_add::{batch_bucketed_add, BucketPosition},
+        };
+
+        let g = GroupAffine::<Parameters>::new(Gx, Gy, false);
+
+        let buckets = 2;
+
+        // these are all [[1]g, [2]g, [3]g, [4]g]
+        let elems = [g, g + g, g + g + g, g + g + g + g];
+
+        // panic on position reuse
+        let mut bucket_positions = [
+            BucketPosition {
+                bucket: 0,
+                position: 0,
+            },
+            BucketPosition {
+                bucket: 0,
+                position: 0,
+            },
+            BucketPosition {
+                bucket: 1,
+                position: 0,
+            },
+            BucketPosition {
+                bucket: 1,
+                position: 1,
+            },
+        ];
+
+        let res = batch_bucketed_add(buckets, &elems, &mut bucket_positions);
+
+        assert_eq!(res.len(), 2);
+        assert_eq!(res[0], g+g);   // elems[0] + elems[0]
+        assert_eq!(res[1], g+g+g); // elems[0] + elems[1]
     }
 }
